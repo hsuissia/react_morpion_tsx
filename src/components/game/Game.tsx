bdc968947b7bb6779719e72483867a01185aec24
Hugo Aka Sawsyn <hugo@carjager.com>
2021-07-08 14:53:08 +0200

    Effet de bouton pour savoir quel étape est active

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -66,7 +66,7 @@ class Game extends Component<{}, MyState> {
             'Revenir au début de la partie';
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button onClick={() => this.jumpTo(move)} className={move === this.state.stepNumber?"button button-active":"button button-primary"}>{desc}</button>
                 </li>
             )
         });
